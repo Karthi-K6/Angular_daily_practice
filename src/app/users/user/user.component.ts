8c4e9a2fd4a688f31b24fba582b8e919fe49356a
= <=>
2024-06-05 23:44:41 +0530

    first-commit

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private router: ActivatedRoute, private route:Router) { }
+
+  ngOnInit() {
+    this.user={
+      id:this.router.snapshot.params['id'],
+      name:this.router.snapshot.params['name']
+    };
+  }
+  onNavigate(){
+    this.route.navigate(['/']);
+  }
+}
